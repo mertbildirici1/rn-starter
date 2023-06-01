f66c0ceac4afa0dab804f518bd0442cd77bc7916
Halit Bildirici <halit.bildirici@duke.edu>
2023-06-01 16:59:16 +0300

    hello?

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = props => {
+const HomeScreen = ({navigation}) => {
 
   return (
   <View>
     <Text style={styles.header}> Getting started with React Native! </Text>
     <Text style = {styles.text}> My name is {name} ! </Text>
     <Button 
-    onPress={() => props.navigation.navigate('Components')}
+    onPress={() => navigation.navigate('Components')}
     title="Go to Components Demo" 
     />
     <Button 
     title="Go to List Demo"
-    onPress={() => props.navigation.navigate('List')}
+    onPress={() => navigation.navigate('List')}
     />
     {/* <TouchableOpacity onPress={() => props.navigation.navigate('List')}>
       <Text> Go to List Demo!</Text>
